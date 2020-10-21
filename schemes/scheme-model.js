6bd0a4b1c6b7fb1fe4000515f27e35000f9279e9
Ariana Habas <65680650+arianahabas@users.noreply.github.com>
2020-10-21 10:33:00 -0500

    MVP complete

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -19,10 +19,34 @@ function findSteps(id){
     .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
 }
 
+async function add(scheme){
+    const [id] = await db('schemes')
+    .insert(scheme)
+
+    return findById(id)
+}
+
+async function update(changes, id){
+    await db('schemes')
+        .where({ id })
+        .update(changes)
+
+    return findById(id)
+}
+
+function remove(id){
+    return db('schemes')
+    .where({ id })
+    .del()
+}
+
 
 
 module.exports = {
     find,
     findById,
     findSteps,
+    add,
+    update,
+    remove
 }
